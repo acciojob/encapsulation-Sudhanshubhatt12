f19a4888c4fb8ca1d875a6ad131485ccd8466a36
Sudhanshu Bhatt <101445771+Sudhanshubhatt12@users.noreply.github.com>
2024-10-23 23:11:13 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
-  
+   public static void main(String[] args) {
+        // Task 3: Create an object of RWOnly
+        RWOnly obj = new RWOnly();
+
+        // Task 4: Trying to access private variable directly
+        // obj.name = "John"; // This will give a compile-time error
+        // System.out.println(obj.name); // This will also give an error
+
+        // Task 5: Using setter and getter methods
+        obj.setName("John");
+        System.out.println(obj.getName()); 
+   }
 }
